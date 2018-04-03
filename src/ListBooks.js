2c07a945e455e59747f24c1eada5b6f55bc5d817
Katie Boedges <kboedges@me.com>
2018-04-02 22:47:48 -0500

    Pulling books from the API instead

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,20 +3,47 @@ import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
     static PropTypes = {
-        books: PropTypes.array.isRequired
+        books: PropTypes.array.isRequired,
+        bookShelf: PropTypes.string.isRequired
     }
 
     render(){
-        const { books } = this.props;
+        const { books, bookShelf } = this.props;
 
-        let showingBooks;
+        let showingBooks = books.filter((book) => book.shelf === bookShelf);
 
         return (
-            <div>Dog</div>
+            <ol className="books-grid">
+                {showingBooks.map((book) => (
+                    <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select value={bookShelf}>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors[0]}</div>
+                        </div>
+                      </li>
+                ))}
+            </ol>
         )
 
     }
 
+}
+
+ListBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    bookShelf: PropTypes.string.isRequired
 }
 
 export default ListBooks
