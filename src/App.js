b9ef99badbf97531b5c2410765d03160ae4867bc
Katherine Boedges <kboedges@me.com>
2018-04-04 17:45:54 -0500

    Adding SearchBooks.js and fixing some formatting issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-/* Using update(book.id, bookShelf) and the BooksAPI, change the bookshelf */
-/* https://reactjs.org/docs/forms.html#why-select-value */
-/**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+/* Check on npm warnings and figure out why the back link doesn't work anymore. Put all of the books html into ListBooks
+use filter to filter by the book shelf for each section and then only use ListBooks once.
+
+Instead of using this state variable to keep track of which page we're on, 
+use the URL in the browser's address bar. This will ensure that
+users can use the browser's back and forward buttons to navigate between
+pages, as well as provide a good URL they can bookmark and share.
+*/
 
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
 
@@ -25,95 +26,94 @@ class BooksApp extends React.Component {
     this.moveBookToShelf = this.moveBookToShelf.bind(this);
   }
 
-  // state = {
-  //   books: [],
-  //   bookShelf: "",
-  //   showSearchPage: false
-  // }
-
   componentDidMount(){
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
   }
 
-  moveBookToShelf(event, book) { 
-    BooksAPI.update(book, event.target.value);
-    console.log(book, event.target.value);
-    console.log(this.state.books);
+  moveBookToShelf(event, book){ 
+    BooksAPI.update(book, event.target.value).then((response) => {
+      console.log(response);
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
+      /* response returns arrays of book ids, find a better way to do this */
+    })
+    // console.log(book, event.target.value);
+    // console.log(this.state.books);
 }
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+render() {
+  return (
+    <div className="app">
+      {this.state.showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <div className="search-books-input-wrapper">
+              {/*
+                NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                You can find these search terms here:
+                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                you don't find a specific author or title. Every search is limited by search terms.
+              */}
+              <input type="text" placeholder="Search by title or author"/>
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                  <ListBooks 
-                    books={this.state.books} 
-                    bookShelf="currentlyReading"
-                    changeShelf={this.moveBookToShelf}
-                  />
-                  </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                <ListBooks 
+                  books={this.state.books} 
+                  bookShelf="currentlyReading"
+                  changeShelf={this.moveBookToShelf}
+                />
                 </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                  <ListBooks 
-                    books={this.state.books} 
-                    bookShelf="wantToRead"
-                    changeShelf={this.moveBookToShelf}
-                  />
-                  </div>
+              </div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                <ListBooks 
+                  books={this.state.books} 
+                  bookShelf="wantToRead"
+                  changeShelf={this.moveBookToShelf}
+                />
                 </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                  <ListBooks 
-                    books={this.state.books} 
-                    bookShelf="read"
-                    changeShelf={this.moveBookToShelf}
-                  />
-                  </div>
+              </div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                <ListBooks 
+                  books={this.state.books} 
+                  bookShelf="read"
+                  changeShelf={this.moveBookToShelf}
+                />
                 </div>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
           </div>
-        )}
-      </div>
-    )
-  }
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 }
 
 export default BooksApp
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,44 +2,43 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired,
+    bookShelf: PropTypes.string.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  }
 
-    static PropTypes = {
-        books: PropTypes.array.isRequired,
-        bookShelf: PropTypes.string.isRequired,
-        changeShelf: PropTypes.func.isRequired
-    }
+  render(){
+    const { books, bookShelf, changeShelf } = this.props;
 
-    render(){
-        const { books, bookShelf, changeShelf } = this.props;
+    let showingBooks = books.filter((book) => book.shelf === bookShelf);
 
-        let showingBooks = books.filter((book) => book.shelf === bookShelf);
-
-        return (
-            <ol className="books-grid">
-                {showingBooks.map((book) => (
-                    <li key={book.id}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select defaultValue={book.shelf} onChange={(event) => changeShelf(event, book)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors[0]}</div>
+    return (
+        <ol className="books-grid">
+            {showingBooks.map((book) => (
+                <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                          <select defaultValue={book.shelf} onChange={(event) => changeShelf(event, book)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
                         </div>
-                      </li>
-                ))}
-            </ol>
-        )
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors[0]}</div>
+                    </div>
+                  </li>
+            ))}
+        </ol>
+    )
 
-    }
+}
 
 }
 
