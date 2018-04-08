a319253c4a300f1b0f0f84d888cd9889f6a4a904
Katie Boedges <kboedges@me.com>
2018-04-07 19:30:18 -0500

    Removing a comment

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,10 +1,3 @@
-/* NOTES: The search from BooksAPI is limited to a particular set of search terms.
-You can find these search terms here:
-https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-you don't find a specific author or title. Every search is limited by search terms.
-*/
-
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
